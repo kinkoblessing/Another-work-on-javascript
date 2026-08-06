339ef7ed07da6c142d664db05608e80b89964c7d
Kinko <kinkoblessing321@gmail.com>
2026-08-06 22:10:59 +0100

    js exercises

--- exercise6.js
+++ exercise6.js
@@ -1,6 +1,13 @@
 function double(n) {
-    return n*2;
+    console.log(n * 2);
 }
 
 // test line - this should print 20, but it won't yet:
+console.log(double(5) + 10);
+
+//the correction after debbuging
+ 
+function double(n) {
+    console.log(n * 2);
+}
 console.log(double(5) + 10);
---
+++ exercise7.js
@@ -0,0 +1,9 @@
+function isOldEnoughToVote(age) {
+    if (age > 18) {
+        console.log("true")
+    } else {
+        console.log("false")
+    }
+}
+
+isOldEnoughToVote(67);
